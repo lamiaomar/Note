99b66344a1f3dcfa85d80f92daa2d0063d427b1f
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2022-02-01 12:19:02 +0300

    initial commit

---
+++ app/src/main/java/com/example/noteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,19 @@
+package com.example.noteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.noteapp.ui.theme.RedOrange
+import java.sql.Timestamp
+
+@Entity
+data class Note(
+    val title : String ,
+    val content : String,
+    val timestamp : Long,
+    val color : Int,
+    @PrimaryKey val id : Int? = null
+){
+    companion object{
+        val noteColor = listOf(RedOrange , )
+    }
+}
