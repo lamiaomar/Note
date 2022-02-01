264285b088d0bfb48878e9147637e473a9788a91
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2022-02-01 16:31:49 +0300

    Note

---
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/util/Screen.kt
@@ -0,0 +1,9 @@
+package com.example.noteapp.feature_note.presentation.util
+
+sealed class Screen(
+    val route : String
+){
+    object NoteScreen : Screen("notes_screen")
+    object AddEditNoteScreen : Screen("add_edit_notes_screen")
+
+}
